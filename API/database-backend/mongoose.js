b8bca82d1c406632b1ee2cc65475cc951416bd29
Juan Santana <juansantanajunior@gmail.com>
2020-11-14 18:37:10 -0500

    MongoDB backend skeleton created

---
+++ API/database-backend/mongoose.js
@@ -0,0 +1,22 @@
+// Handles connection logic to MongoDB
+
+const mongoose = require('mongoose');
+
+// Use JS global promise instead
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/TaskListManagerApp', { useNewUrlParser: true }).then(() => {
+    console.log("Connected to MongoDB!!!");
+}).catch((e) => {
+    console.log("ERROR: Unable to connect to MongoDB");
+    console.log(e);
+});
+
+// Prevent deprectation warnings
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+// Export mongoose object
+module.exports = {
+    mongoose
+};
