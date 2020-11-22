e8583e002393316b547caa3edebd51177bb3c42e
Juan Santana <juansantanajunior@gmail.com>
2020-11-21 19:53:09 -0500

    Update app.js

--- API/app.js
+++ API/app.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json()); // Passes req body of http request
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE');
         return res.status(200).json({});
     };
     next();
