763028889334d37af3aede48edb3dd09c9b2df2b
Juan Santana <juansantanajunior@gmail.com>
2020-11-23 15:04:35 -0500

    Instance and Helper methods for user model

--- API/database-backend/mongoose-models/user.model.js
+++ API/database-backend/mongoose-models/user.model.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto'); 
+
+// JWT Secret
+const jwtSecret = "4le6e0ivh9wtrrgbp911j53t41e3ssyixlef13g4rw379z1pzra9oc8niq8w";
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -24,3 +30,91 @@ const UserSchema = new mongoose.Schema({
         }
     }]
 });
+
+// Instance Methods
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    // Returns the doc except password and sessions (password and sessions should be priavte)
+    return _.omit(userObject, ['password', 'sessions']);
+}
+
+// Generates access token
+
+UserSchema.methods.generateAccessAuthToken = function () {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        // Creates JSON Web Token
+        jwt.sign({ _id: user._id.toHexString() }, jwtSecret, { expiresIn: "15m" }, (err, token) => {
+            if (!err) { // returns JSON Web Token
+                resolve(token);
+            } else {
+                // rejects webtoken; there is an error
+                reject();
+            }
+        })
+    })
+}
+
+UserSchema.methods.generateRefreshAuthToken = function () {
+    // Generates 64byte hex string; Doesn't save it to the database; saveSessionToDatabase() does that.
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(64, (err, buf) => {
+            if (!err) {
+                // no error; gets token from the buffer in hex format
+                let token = buf.toString('hex');
+
+                return resolve(token);
+            }
+        })
+    })
+}
+
+UserSchema.methods.createSession = function () {
+    let user = this;
+
+    return user.generateRefreshAuthToken().then((refreshToken) => {
+        return saveSessionToDatabase(user, refreshToken);
+    }).then((refreshToken) => {
+        // saved to database successfully
+        // now return the refresh token
+        return refreshToken;
+    }).catch((e) => {
+        return Promise.reject('Failed to save session to database.\n' + e);
+    })
+}
+
+// Model/Static Methods
+
+
+
+
+// Helper Methods
+
+let saveSessionToDatabase = (user, refreshToken) => {
+    // Saves session to database
+    return new Promise((resolve, reject) => {
+        let expiresAt = generateRefreshTokenExpiryTime();
+
+        user.sessions.push({ 'token': refreshToken, expiresAt });
+
+        user.save().then(() => {
+            // Session is successfully saved
+            return resolve(refreshToken);
+        }).catch((e) => {
+            reject(e);
+        });
+    })
+}
+
+let generateRefreshTokenExpiryTime = () => {
+    let daysUntilExpire = "10"; //10 days until refresh token expires
+    let secondsUntilExpire = ((daysUntilExpire * 24) * 60) * 60;
+    return ((Date.now() / 1000) + secondsUntilExpire);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User }
