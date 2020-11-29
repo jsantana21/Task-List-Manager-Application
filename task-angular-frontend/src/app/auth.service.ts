b96f89c998b55b7c55f398bd16865bfb0b0fdf0c
Juan Santana <juansantanajunior@gmail.com>
2020-11-29 14:18:49 -0500

    function for Log In button made

--- task-angular-frontend/src/app/auth.service.ts
+++ task-angular-frontend/src/app/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { WebRequestService } from './web-request.service';
