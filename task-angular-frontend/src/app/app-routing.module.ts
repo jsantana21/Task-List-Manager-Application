1b15220e2f6b4795e18b57cc0f8bf8caf37c783e
Juan Santana <juansantanajunior@gmail.com>
2020-12-09 22:22:53 -0500

    sign up html done

--- task-angular-frontend/src/app/app-routing.module.ts
+++ task-angular-frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { TaskViewerComponent } from './pages/task-viewer/task-viewer.component';
 import { NewTaskListComponent } from './pages/new-task-list/new-task-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'tasklists', pathMatch: 'full' },
   { path: 'new-task-list', component: NewTaskListComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
   { path: 'tasklists', component: TaskViewerComponent },
   { path: 'tasklists/:tasklistId', component: TaskViewerComponent },
   { path: 'tasklists/:tasklistId/new-task', component: NewTaskComponent }
